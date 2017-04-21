dc0406bcea92cd919499f8d6912f5b9590ecdd54
Walter Medvedeo <wmedvedeo@gmail.com>
2017-04-21 09:58:42 +0200

    Configuration for kie-wb

---
+++ bin/clear-jboss.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+
+rm -Rf ./.errai
+
+rm -Rf ./.index
+
+rm -Rf ./.niogit
+
+rm -Rf ./.security
+
+rm -Rf ./repositories
+
+rm -Rf ./.kie-wb-playground
+
+rm -Rf ../standalone/data/*
+
+rm -Rf ../standalone/log/*
+
+rm -Rf ../standalone/tmp/*
+
+rm -Rf ../standalone/configuration/standalone_xml_history
+
+
